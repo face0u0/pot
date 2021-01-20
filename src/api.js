e699841cc437cfc6a1080c683dc2298aa9709f8a
face0u0 <me@face0u0.net>
2021-01-21 01:21:20 +0900

    add method alias

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 import { Container } from "./domain/container.js";
 import { NormalProvider } from "./domain/provider.js";
 import { Recipe } from "./domain/recipe.js";
+import { UnexpectedError } from "./domain/throwable.js";
+import { concat } from "./util/arrays.js";
 
 export class Pot {
 
@@ -8,6 +10,11 @@ export class Pot {
 
         /** @type {Array<Recipe<any>>} */
         this.__recipes = []
+
+        /** @type {Object<string, string>} */
+        this.__aliasesHashMap = {}
+
+        /** @type {Container|null} */
         this.__container = null
     }
 
@@ -21,8 +28,18 @@ export class Pot {
         this.__recipes.push(new Recipe(name, clazz, dependencies, new NormalProvider(clazz)))
     }
 
+    /**
+     * 
+     * @param {string} name 
+     * @param {string} preDefinedName 
+     */
+    alias(name, preDefinedName){
+        this.__aliasesHashMap[name] = preDefinedName
+    }
+
     resolve(){
-        this.__container = new Container(this.__recipes)
+        const aliasRecipes = this.__resolveAliases()
+        this.__container = new Container(concat(this.__recipes, aliasRecipes) )
     }
 
     /**
@@ -34,6 +51,46 @@ export class Pot {
             return null
         }
         return this.__container.create(name)
+    }
+
+    /**
+     * @returns {Array<Recipe<any>>}
+     */
+    __resolveAliases(){
+        const recipes = Object.keys(this.__aliasesHashMap).map(aliasName => {
+            const preDefinedName = this.__aliasesHashMap[aliasName]
+            const recipe = this.__findRecipe(preDefinedName)
+            return new Recipe(aliasName, recipe.clazz, recipe.dependencies, recipe.provider)
+        })
+        return recipes
+    }
+
+    /**
+     * 
+     * @param {string} name
+     */
+    __findRecipe(name){
+        const recipes = this.__recipes.filter(recipe => recipe.name === name)
+        switch(recipes.length){
+            case 0:
+                throw new RecipeUnresolvedError(name)
+            case 1:
+                return recipes[0]
+            default:
+                throw new UnexpectedError()
+        }
+    }
+}
+
+export class RecipeUnresolvedError extends Error {
+
+    /**
+     * 
+     * @param {string} recipeName 
+     */
+    constructor(recipeName) {
+        super(`recipe ${recipeName} cannot resolved`);
+        this.name = "RecipeUnresolvedError";
     }
 }
 
--- src/util/arrays.js
+++ src/util/arrays.js
@@ -6,4 +6,18 @@
  */
 export const includes = (array, val) => {
     return array.indexOf(val) !== -1
-}   
+}
+
+/**
+ * @param {Array<Array<T>>} arrays
+ * @returns {Array<T>}
+ * @template T
+ */
+export const concat = (...arrays) => {
+    /** @type {Array<T>} */
+    let concatted = []
+    arrays.forEach(array => {
+        concatted = concatted.concat(array)
+    })
+    return concatted
+}

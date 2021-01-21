a01062c04bcdf40cfa129f141d329b7bb7089fcf
face0u0 <me@face0u0.net>
2021-01-21 16:54:32 +0900

    build setting

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
 
-    mode: 'production',
+    mode: 'development',
 
     entry: path.resolve(__dirname, "index.js"),
     
@@ -12,6 +12,4 @@ module.exports = {
         library: "IocPot",
         libraryTarget: 'umd'
     },
-
-    devtool: "source-map"
 }

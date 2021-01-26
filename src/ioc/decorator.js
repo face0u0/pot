c2dedb78eb9dd557046b08702e0cda7575b3efbc
face0u0 <me@face0u0.net>
2021-01-27 02:35:58 +0900

    make once do decorator

--- src/ioc/decorator.js
+++ src/ioc/decorator.js
@@ -17,6 +17,41 @@ export class Decorator{
 }
 
 /**
+ * 
+ * @abstract
+ * @implements {Decorator<T>}
+ * @template T
+ */
+export class ExactryOnceDecorator{
+
+    constructor(){
+
+        /** @type {Array<T>} */
+        this.prevs = []
+    }
+
+    /**
+     * 
+     * @param {T} instance 
+     */
+    patch(instance){
+        const isDecorated = this.prevs.some(t => (typeof t === "object") && t === instance)
+        if(!isDecorated){
+            this.prevs.push(instance)
+            this.onceDo(instance)
+        }
+    }
+    /**
+     * 
+     * @param {T} instance
+     * @abstract 
+     */
+    onceDo(instance){
+        throw new NotImplementedError()
+    }
+}
+
+/**
  * @implements {Decorator<Instance>}
  */
 export class SilentDecorator{
